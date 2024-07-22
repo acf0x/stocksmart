d912242d09acfa4c625f8c2678b2eb2f1efcdcc6
Roberto Hidalgo <robehimi2000@gmail.com>
2024-07-22 17:31:01 +0200

    POST crear stock

--- StockSmartAPI/app.py
+++ StockSmartAPI/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request
 from azure.cosmos import CosmosClient, exceptions, PartitionKey
+import uuid
 
 app = Flask(__name__)
 
 # Configuración de Cosmos DB
-COSMOS_URL = ''
-COSMOS_KEY = ''
-DATABASE_NAME = ''
-CONTAINER_NAME = ''
+COSMOS_URL = 'https://democosmospd.documents.azure.com/'
+COSMOS_KEY = '0xeX5OLpoabjFyt21r1k0sPgh7ITL84wUMNSpqoC7u8dnwQNgb5XlxMqlRIuKlXphvtlKEhQ57V3ACDb4dyRNw=='
+DATABASE_NAME = 'democosmospd'
+CONTAINER_NAME = 'democontainerpd'
 
 # Inicializar el cliente de Cosmos DB
 client = CosmosClient(COSMOS_URL, COSMOS_KEY)
@@ -26,11 +27,38 @@ def get_stocks():
     ))
     return jsonify(items)
 
-@app.route('/stocks', methods=['POST'])
+@app.route('/productos/nuevo', methods=['POST'])
 def create_stock():
     data = request.get_json()
-    container.create_item(body=data)
-    return jsonify(data), 201
+    
+    # Validar y extraer los parámetros
+    descripcion = data.get('descripcion')
+    proveedor = data.get('proveedor')
+    categoria = data.get('categoria')
+    cantidades_por_unidad = data.get('cantidades_por_unidad')
+    precio = data.get('precio')
+    stock = data.get('stock')
+    stock_pedido = data.get('stock_pedido')
+    nivel = data.get('nivel')
+    descuentos = data.get('descuentos')
+
+    # Crear el nuevo ítem con los parámetros
+    nuevo_item = {
+        'id': str(uuid.uuid4()),  # Generar un ID único
+        'descripcion': descripcion,
+        'proveedor': proveedor,
+        'categoria': categoria,
+        'cantidades_por_unidad': cantidades_por_unidad,
+        'precio': precio,
+        'stock': stock,
+        'stock_pedido': stock_pedido,
+        'nivel': nivel,
+        'descuentos': descuentos
+    }
+
+    # Insertar el nuevo ítem en el contenedor
+    container.create_item(body=nuevo_item)
+    return jsonify(nuevo_item), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
