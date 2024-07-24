747169d67f0fc6aa076b576af07705c406f96764
acf0x <alcasfer96@gmail.com>
2024-07-24 21:01:55 +0200

    Arreglo de métodos get_products() y get_products(id) e introducción de secrets de Azure Key Vault

--- StockSmartAPI/app.py
+++ StockSmartAPI/app.py
@@ -1,31 +1,50 @@
 from flask import Flask, jsonify, request
 from azure.cosmos import CosmosClient, exceptions, PartitionKey
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 import uuid
 
 app = Flask(__name__)
 
 # Configuración de Cosmos DB
-COSMOS_URL = ''
-COSMOS_KEY = ''
-DATABASE_NAME = ''
-CONTAINER_NAME = '' 
+COSMOS_URL = "https://cosmosdbpd.documents.azure.com/"
+KEY_VAULT_URL = "https://demokeyvaultpd2.vault.azure.net/"
+SECRET_NAME = "cosmoskey"
 
+# Obtener la clave de Cosmos DB desde Azure Key Vault
+credential = DefaultAzureCredential()
+secret_client = SecretClient(KEY_VAULT_URL, credential)
+COSMOS_KEY = secret_client.get_secret(SECRET_NAME).value
+
+DATABASE_NAME = "stockdb"
+CONTAINER_NAME = "products"
 # Inicializar el cliente de Cosmos DB
 client = CosmosClient(COSMOS_URL, COSMOS_KEY)
 database = client.create_database_if_not_exists(DATABASE_NAME)
-container = database.create_container_if_not_exists(CONTAINER_NAME)
+container = database.create_container_if_not_exists(CONTAINER_NAME, PartitionKey('/ProductID'))
 
 @app.route('/data/products', methods=['GET'])
 def get_products():
     query = "SELECT * FROM c"
-    items = list(container.query_items(query))
-    return jsonify(items)
+    try:
+        items = list(container.query_items(query, enable_cross_partition_query=True))
+        if not items:
+            return jsonify({"message": "No se encontraron productos."}), 404
+        return jsonify(items)
+    except exceptions.CosmosHttpResponseError as e:
+        return jsonify({"Error": str(e)}), 500
 
 @app.route('/data/products/<id>', methods=['GET'])
 def get_product(id):
-    query = f"SELECT * FROM c WHERE c.ProductID EQUALS {id}"
-    item = container.query_item(query)
-    return jsonify(item)
+    query = f"SELECT * FROM c WHERE c.ProductID = '{id}'"
+    try:
+        items = container.query_items(query)
+        item = next(items, None)  # Obtener el primer ítem o None si no hay resultados
+        if item is None:
+            return jsonify({"message": "Producto no encontrado."}), 404
+        return jsonify(item)
+    except exceptions.CosmosHttpResponseError as e:
+        return jsonify({"Error": str(e)}), 500
 
 @app.route('/products/new', methods=['POST'])
 def create_product():
